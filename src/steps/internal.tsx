8e2871ef6d3f332376d75fb315829d18ea215600
Timo <gasda@amazon.com>
2025-10-22 11:38:09 +0200

    feat: Add horizontal layout and custom rendering to steps

--- src/steps/internal.tsx
+++ src/steps/internal.tsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import clsx from 'clsx';
 
+import { BoxProps } from '../box/interfaces';
+import InternalBox from '../box/internal';
 import { InternalBaseComponentProps } from '../internal/hooks/use-base-component';
 import { SomeRequired } from '../internal/types';
 import StatusIndicator from '../status-indicator/internal';
@@ -12,15 +14,66 @@ import styles from './styles.css.js';
 
 type InternalStepsProps = SomeRequired<StepsProps, 'steps'> & InternalBaseComponentProps;
 
-const InternalStep = ({ status, statusIconAriaLabel, header, details }: StepsProps.Step) => {
+const statusToColor: Record<StepsProps.Status, BoxProps.Color> = {
+  error: 'text-status-error',
+  warning: 'text-status-warning',
+  success: 'text-status-success',
+  info: 'text-status-info',
+  stopped: 'text-status-inactive',
+  pending: 'text-status-inactive',
+  'in-progress': 'text-status-inactive',
+  loading: 'text-status-inactive',
+};
+
+const CustomStep = ({
+  step,
+  orientation,
+  renderStep,
+}: {
+  step: StepsProps.Step;
+  orientation: StepsProps.Orientation;
+  renderStep: Required<StepsProps>['renderStep'];
+}) => {
+  const { status, statusIconAriaLabel } = step;
+  const { header, details, icon } = renderStep(step);
+  return (
+    <li className={styles.container}>
+      <div className={styles.header}>
+        {icon ? icon : <StatusIndicator type={status} iconAriaLabel={statusIconAriaLabel} />}
+        {orientation === 'vertical' ? header : <hr className={styles.connector} role="none" />}
+      </div>
+      {orientation === 'vertical' ? (
+        <hr className={styles.connector} role="none" />
+      ) : (
+        <div className={styles['horizontal-header']}>{header}</div>
+      )}
+      {details && <div className={styles.details}>{details}</div>}
+    </li>
+  );
+};
+
+const InternalStep = ({
+  status,
+  statusIconAriaLabel,
+  header,
+  details,
+  orientation,
+}: StepsProps.Step & { orientation: StepsProps.Orientation }) => {
   return (
     <li className={styles.container}>
       <div className={styles.header}>
         <StatusIndicator type={status} iconAriaLabel={statusIconAriaLabel}>
-          {header}
+          {orientation === 'vertical' && header}
         </StatusIndicator>
+        {orientation === 'horizontal' && <hr className={styles.connector} role="none" />}
       </div>
-      <hr className={styles.connector} role="none" />
+      {orientation === 'vertical' ? (
+        <hr className={styles.connector} role="none" />
+      ) : (
+        <div className={styles['horizontal-header']}>
+          <InternalBox color={statusToColor[status]}>{header}</InternalBox>
+        </div>
+      )}
       {details && <div className={styles.details}>{details}</div>}
     </li>
   );
@@ -28,29 +81,40 @@ const InternalStep = ({ status, statusIconAriaLabel, header, details }: StepsPro
 
 const InternalSteps = ({
   steps,
+  orientation,
+  renderStep,
   ariaLabel,
   ariaLabelledby,
   ariaDescribedby,
   __internalRootRef,
   ...props
-}: InternalStepsProps) => {
+}: SomeRequired<InternalStepsProps, 'orientation'>) => {
   return (
-    <div {...props} className={clsx(styles.root, props.className)} ref={__internalRootRef}>
+    <div
+      {...props}
+      className={clsx(styles.root, props.className, orientation === 'horizontal' ? styles.horizontal : styles.vertical)}
+      ref={__internalRootRef}
+    >
       <ol
         className={styles.list}
         aria-label={ariaLabel}
         aria-labelledby={ariaLabelledby}
         aria-describedby={ariaDescribedby}
       >
-        {steps.map((step, index) => (
-          <InternalStep
-            key={index}
-            status={step.status}
-            statusIconAriaLabel={step.statusIconAriaLabel}
-            header={step.header}
-            details={step.details}
-          />
-        ))}
+        {steps.map((step, index) =>
+          renderStep ? (
+            <CustomStep key={index} orientation={orientation} step={step} renderStep={renderStep} />
+          ) : (
+            <InternalStep
+              key={index}
+              status={step.status}
+              statusIconAriaLabel={step.statusIconAriaLabel}
+              header={step.header}
+              details={step.details}
+              orientation={orientation}
+            />
+          )
+        )}
       </ol>
     </div>
   );
